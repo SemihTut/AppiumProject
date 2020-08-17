0b34b09dd890532d2cf12aa9253c2ac52604a969
SemihTut <yalaantv@gmail.com>
2020-08-17 10:10:30 +0300

    Drag&Drop

---
+++ src/test/java/AppiumTests/DragAndDrop.java
@@ -0,0 +1,29 @@
+package AppiumTests;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidElement;
+import org.testng.annotations.Test;
+
+import static io.appium.java_client.touch.LongPressOptions.*;
+import static io.appium.java_client.touch.offset.PointOption.*;
+import static io.appium.java_client.touch.LongPressOptions.*;
+import static io.appium.java_client.touch.TapOptions.*;
+import static io.appium.java_client.touch.LongPressOptions.*;
+import static io.appium.java_client.touch.offset.ElementOption.*;
+import java.net.MalformedURLException;
+
+public class DragAndDrop extends Base{
+
+    @Test
+    public void dragAndDrop() throws MalformedURLException {
+
+        driver=Capabilities();
+        navigateTo("Views","Drag and Drop");
+        TouchAction ta = new TouchAction(driver);
+        AndroidElement firstDot = driver.findElementById("io.appium.android.apis:id/drag_dot_1");
+        AndroidElement thirdDot = driver.findElementById("io.appium.android.apis:id/drag_dot_3");
+
+       // ta.longPress(longPressOptions().withElement(element(firstDot))).moveTo(element(thirdDot)).release().perform();
+        ta.longPress(element(firstDot)).moveTo(element(thirdDot)).release().perform();
+    }
+}
---
+++ src/test/java/AppiumTests/KeysOfMobile.java
@@ -0,0 +1,31 @@
+package AppiumTests;
+
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.AndroidKey.*;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.android.nativekey.PressesKey;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+
+import static io.appium.java_client.touch.offset.PointOption.*;
+import static io.appium.java_client.touch.TapOptions.*;
+import static io.appium.java_client.touch.LongPressOptions.*;
+import static io.appium.java_client.touch.offset.ElementOption.*;
+
+public class KeysOfMobile extends Base{
+    @Test
+    public void keysEvent() throws MalformedURLException {
+
+        driver=Capabilities();
+        navigateTo("Views","Drag and Drop");
+        ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.BACK));
+
+
+    }
+
+}
